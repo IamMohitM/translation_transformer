2f59aa1ab7037890483798ff9aa17cf42c9c581c
Mohit Motwani <mohitmotwani54@gmail.com>
2023-04-28 16:05:34 +0000

    backup commit

--- test.py
+++ test.py
@@ -1,53 +1,61 @@
-from transformer.translate import translate
-from transformer.transformer import EncoderDecoder
-from transformer.prepare_data import build_vocab, get_dataset_iter, load_tokenizer
 import torch
+import cv2
+import numpy as np
+from PIL import Image
+from torchvision import transforms
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+from transformer.vision_transformer import ViT
+
+img_size = (56, 56)
+
+val_augment = transforms.Compose(
+    [
+        transforms.Resize(img_size),
+        transforms.PILToTensor(),
+        lambda x: x.float(),
+        lambda x: x / 255.0,
+    ]
+)
+
+config = {
+    "patch_size": 8,
+    "embed_dim": 512,
+    "mlp_num_hidden": 1024,
+    "num_heads": 16,
+    "num_blocks": 8,
+    "emb_dropout": 0.1,
+    "block_dropout": 0.1,
+    "use_bias": True,
+    "img_size": img_size,
+    "image_channels": 1,
+}
+
+checkpoint_path = "checkpoints/vit_model.pth"
+model = ViT(**config)
+
+model.load_state_dict(torch.load(checkpoint_path)['state_dict'])
+model.eval()
+model = model.cuda()
+
+
+def preprocess_image(image):
+    """Simply the function to preprocess the image"""
+
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    image = Image.fromarray(image)
+    image = val_augment(image)
+    image = image.unsqueeze(0)
+    image = image.cuda()
+    return image
+
+
+def predict(image):
+    image = preprocess_image(image)
+    preds = model(image)
+    probs = torch.nn.functional.softmax(preds, dim=1)
+    return probs
+
+
 
 if __name__ == "__main__":
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    num_heads = 8
-    embed_dim = 512
-    ffn_num_hidden = 512
-    num_hidden = 64  # dq = dk = dv = num_hiddens
-    num_blocks = 6
-    dropout = 0.1
-
-    dataset = get_dataset_iter("train", ["de", "en"])
-
-    source_tokenizer = load_tokenizer("de")
-    target_tokenizer = load_tokenizer("en")
-    source_vocab_path = "checkpoints/source_vocab.pth"
-    target_vocab_path = "checkpoints/target_vocab.pth"
-    source_vocab = torch.load(source_vocab_path)
-    target_vocab = torch.load(target_vocab_path)
-    # source_vocab = build_vocab(source_tokenizer, dataset, "de")
-    # target_vocab = build_vocab(target_tokenizer, dataset, "en")
-
-    model = EncoderDecoder(
-        embed_dim=embed_dim,
-        num_hidden=num_hidden,
-        ffn_hidden=ffn_num_hidden,
-        num_heads=num_heads,
-        num_blocks=num_blocks,
-        source_vocab_size=len(source_vocab),
-        target_vocab_size=len(target_vocab),
-    ).to(device)
-
-    checkpoint_path = "checkpoints/model.pt"
-
-    model.load_state_dict(torch.load(checkpoint_path)["state_dict"])
-    model.eval()
-
-    print(
-        translate(
-            model,
-            source_vocab,
-            source_tokenizer,
-            target_vocab,
-            target_tokenizer,
-            # "Ein schwarzer Hund und ein gefleckter Hund kämpfen.",
-            "Vier Typen, von denen drei Hüte tragen und einer nicht, springen oben in einem Treppenhaus.",
-        )
-    )
+    ...
